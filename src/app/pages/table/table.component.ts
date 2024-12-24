34c9f1f2d142717762ced469ccb7dfc59d82bf53
shu <a.0066715@gmail.com>
2024-12-24 16:16:03 +0800

    加上搜尋框&換頁方法

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule, formatDate } from '@angular/common';
 import { NzTableModule,  NzTableFilterFn, NzTableFilterList, NzTableSortFn } from 'ng-zorro-antd/table';
-import { TableService } from './table.service';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzModalModule, NzModalRef } from 'ng-zorro-antd/modal';
 import { NzModalService } from 'ng-zorro-antd/modal';
@@ -9,6 +8,10 @@ import { FormComponent } from './form/form.component';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { addDays, format, subDays } from 'date-fns';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { FormsModule, FormBuilder, FormControl, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { tr } from 'date-fns/locale';
 
 interface ItemData {
   id: number;
@@ -33,7 +36,8 @@ interface ColumnItem {
     selector: 'app-table',
     imports: [
         CommonModule, NzTableModule, NzButtonModule,
-        NzModalModule, NzDividerModule
+        NzModalModule, NzDividerModule, NzFormModule,
+        FormsModule, ReactiveFormsModule, NzInputModule
     ],
     templateUrl: './table.component.html',
     styleUrl: './table.component.css'
@@ -60,7 +64,7 @@ export class TableComponent implements OnInit {
     },
     {
       name: '建立時間',
-      width: '550px',
+      width: '',
       position: null,
       sortFn: (a: ItemData, b: ItemData) => new Date(a.createDate).getTime() - new Date(b.createDate).getTime(),
       filterMultiple: false,
@@ -71,7 +75,7 @@ export class TableComponent implements OnInit {
     },
     {
       name: '最後修改時間',
-      width: '550px',
+      width: '',
       position: null,
       sortFn: (a: ItemData, b: ItemData) => new Date(a.lastUpdate).getTime() - new Date(b.lastUpdate).getTime(),
       filterMultiple: false,
@@ -117,20 +121,166 @@ export class TableComponent implements OnInit {
     },
     {
       id: 2,
+      name: 'ccc2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
       name: '檔案2',
       createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
       lastUpdate: this.formattedDate,
       done: false
-    }
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: true
+    },
+    {
+      id: 2,
+      name: 'cccc',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: 'aaa',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: true
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: true
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: 'ddd',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
+    {
+      id: 2,
+      name: '檔案2',
+      createDate: format(addDays(new Date(), 1), 'yyyy-MM-dd'),
+      lastUpdate: this.formattedDate,
+      done: false
+    },
   ];
 
+  pageIndex : number = 1;
+
+  pageSize : number = 10;
+
+  filteredLists: any[] = [];
+
+  total : number = this.listOfData.length;
+
+  displayedList: any[] = this.listOfData;
+
+  totalPages: number = Math.ceil(this.total / this.pageSize);
+
+  form: FormGroup = new FormGroup({});
+
   constructor(
-              private tableService: TableService,
               private modalService: NzModalService,
-              private message: NzMessageService
+              private message: NzMessageService,
+              private fb: FormBuilder
             ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      search: [null],
+    });
+    this.filteredLists = this.listOfData;
+    this.updateDisplayedList();
+  }
 
   /**
    * 打開新增視窗
@@ -155,15 +305,57 @@ export class TableComponent implements OnInit {
     });
   }
 
-  downloadFile() {
-    const url = '';
-    this.tableService.downloadFile(url).subscribe((blob) => {
-      const a = document.createElement('a');
-      const objectUrl = URL.createObjectURL(blob);
-      a.href = objectUrl;
-      a.download = 'downloadedFile';
-      a.click();
-      URL.revokeObjectURL(objectUrl);
-    });
+  /**
+   * 按下enter搜尋列表
+   * @param event
+   */
+  searchList(event: any): any {
+    if (event.key === 'Enter') {
+      const searchTerm = event.target.value.trim();
+      const startIndex = (this.pageIndex - 1) * 12;
+      const endIndex = Math.min(startIndex + 12, this.total);
+      const currentPageItems = this.listOfData.slice(startIndex, endIndex);
+
+      if (searchTerm === '') {
+        this.filteredLists = currentPageItems;
+      }
+      else {
+        this.filteredLists = currentPageItems.filter((item) =>
+          item.name.indexOf(searchTerm) !== -1
+        );
+      }
+      this.updateDisplayedList();
+    }
   }
+
+  changePage(newPageIndex: number, newPageSize?: number): void {
+    if (newPageSize) {
+      this.pageSize = newPageSize;
+    }
+    this.totalPages = Math.ceil(this.total / this.pageSize);
+
+    // 確保 pageIndex 不會超出新的 totalPages
+    if (newPageIndex > this.totalPages) {
+      newPageIndex = this.totalPages;
+    }
+
+    if (newPageIndex < 1) {
+      newPageIndex = 1;
+    }
+
+    this.pageIndex = newPageIndex;
+    const startIndex = (this.pageIndex - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.displayedList = this.listOfData.slice(startIndex, endIndex);
+  }
+
+   /**
+   *根據當前頁碼和已篩選的列表，更新顯示的列表
+  */
+   updateDisplayedList() {
+    const startIndex = (this.pageIndex - 1) * 12;
+    const endIndex = Math.min(startIndex + 12, this.filteredLists.length);
+    this.displayedList = this.filteredLists.slice(startIndex, endIndex);
+  }
+
 }
